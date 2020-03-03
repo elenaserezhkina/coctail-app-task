c83faa630cf43396561f455e57f6d23bbe4fd4b4
Elena <anel92r@gmail.com>
2020-03-03 17:28:14 +0100

    homepage

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Card.scss";
 
-function Card() {
+function Card({ cardTitle }) {
   return (
     <div className="card-container">
       <div className="CardBackground">
-        <h3>Cocktail Name</h3>
+        <h3>{cardTitle}</h3>
         <p>Ingredients</p>
         <div className="mainIngredients">
           <div className="buttonIngredient">
---
+++ src/Pages/Hompage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "../Component/Card";
+
+function Hompage() {
+  return (
+    <div>
+      <Card cardTitle="name" cardImg=""></Card>
+    </div>
+  );
+}
